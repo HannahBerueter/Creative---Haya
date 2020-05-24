68b923d21cb8532f09a9800a7c1c5146dfb4085f
Yael Fassbind <yael.fassbind@hotmail.com>
2020-05-24 14:25:19 +0200

    navigation fix

--- src/views/navigation/navigation.js
+++
@@ -1,7 +0,0 @@
-function redirect(pageName) {
-    var currentDirectory = window.location.pathname.split('/').slice(0, -1).join('/');
-    console.log(currentDirectory);
-    var aimDirectory =  currentDirectory.replace("navigation", pageName + "/" + pageName + ".html");
-    console.log(aimDirectory);
-    window.location.href = aimDirectory;
-};
